27f642ecdae14da74cbf14191f234e668191485f
Domenico Leone Luciani <domenicoleoneluciani@gmail.com>
2013-08-11 18:19:21 +0200

    Aggiornato config

--- config/index.js
+++ config/index.js
@@ -11,8 +11,6 @@ config.web = {};
 //
 // Declare Configuration of Database
 //
-config.jugglingdb.host = 'localhost';
-config.jugglingdb.port = 3306; //Insert correct port here
 config.jugglingdb.user = 'root';//Insert correct user here
 config.jugglingdb.pass = 'root'; //Insert correct pass here
 config.jugglingdb.db = 'NodeBlog'; //Insert correct name of database
@@ -24,7 +22,7 @@ config.web.port = 3000;
 config.web.admin = ['DLion','unsign3d'];
 config.web.secret_cookie = "NodeBlogIsFuckingAwesome";
 config.web.secret_session = "NodeBlogIsMoreFuckinAwesomeThanYou";
-
+config.web.secret_salt = "NodeBlogSaltPass";
 //
 // Exports
 //
