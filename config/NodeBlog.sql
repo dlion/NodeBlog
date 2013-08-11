09f49dd8c13aab11381852921af42271d9f6eb17
Domenico Luciani <domenicoleoneluciani@gmail.com>
2013-08-11 17:16:20 +0200

    Aggiunto il mio user e pulita la tabella

--- config/NodeBlog.sql
+++ config/NodeBlog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generato il: Ago 11, 2013 alle 12:53
+-- Generato il: Ago 11, 2013 alle 15:15
 -- Versione del server: 5.5.31-MariaDB-log
 -- Versione PHP: 5.4.17
 
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `categorie` (
 
 CREATE TABLE IF NOT EXISTS `post` (
   `id` bigint(11) NOT NULL AUTO_INCREMENT,
-  `data` date NOT NULL,
+  `data` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `id_categoria` tinyint(11) NOT NULL,
   `testo` text NOT NULL,
   `autore` tinyint(11) NOT NULL,
@@ -59,11 +59,17 @@ CREATE TABLE IF NOT EXISTS `utenti` (
   `nick` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `level` tinyint(11) NOT NULL,
   `hash` varchar(255) NOT NULL,
-  `last_login` date NOT NULL,
+  `last_login` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dump dei dati per la tabella `utenti`
+--
+
+INSERT INTO `utenti` (`id`, `nick`, `email`, `password`, `hash`, `last_login`) VALUES
+(1, 'DLion', 'domenicoleoneluciani@gmail.com', '46bf7549e93d8716756567c2882284f79ead7d0d', '4ba84e9293cb54e01c761c41d4ea8fa5b0b4dcbd', '2013-08-11 02:25:12');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
