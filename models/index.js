7e2eff476444a3e029965e62d11ef3aea85ded69
Domenico Luciani <domenicoleoneluciani@gmail.com>
2013-09-19 12:51:21 +0200

    Fatto prove con mongodb

--- models/index.js
+++ models/index.js
@@ -1,6 +1,42 @@
-var mongodb = require('mongodb').MongoClient;
-/*
-mongodb.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
-        if(err) throw err;
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/nodeblog');
 
-  */      
+// Definisco il modello
+var Articolo = mongoose.model('Articolo', {
+	titolo: String,
+	testo: String,
+	autore: String,
+	tag: {
+		nome: String
+	},
+	commenti: {
+		nome: String,
+		email: String,
+		commento: String,
+		creato_il: { type: Date, default: Date.now() }
+	},
+	creato_il: { type: Date, default: Date.now() }
+});
+
+//Inserisco un articolo
+
+var post = new Articolo({
+	titolo: "Un titolo",
+	testo: "Nella grande casa blu",
+	autore: "DLion",
+	tag: {
+		nome: "Prova"
+	},
+	commenti: {
+		nome: "Estraneo",
+		email: "estraneo@gmail.ti",
+		commento: "Commento produttivo"
+	}
+});
+
+
+// Trovo tutti i post di DLion
+Articolo.find({ autore: "DLion" },function(err, dato) {
+		console.log("QUI STAMPO: "+dato);
+		mongoose.disconnect();
+});
